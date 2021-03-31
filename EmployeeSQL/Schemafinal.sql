c12ee9bcef13a27150746c95f7f601fa37b5f69b
FSR <your_email@fpshami@gmail.com>
2021-03-30 22:43:01 -0500

    Vfinal

--- EmployeeSQL/Schemafinal.sql
+++ EmployeeSQL/Schemafinal.sql
@@ -1,4 +1,4 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
@@ -25,23 +25,38 @@ CREATE TABLE "employees" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL
+    "dept_no" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "dept_managers" (
     "dept_no" VARCHAR   NOT NULL,
-    "emp_no" INT   NOT NULL
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_dept_managers" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 CREATE TABLE "saleries" (
     "emp_no" INT   NOT NULL,
-    "salary" BIGINT   NOT NULL
+    "salary" BIGINT   NOT NULL,
+    CONSTRAINT "pk_saleries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "titles" (
     "emp_title_id" VARCHAR   NOT NULL,
-    "title" VARCHAR   NOT NULL
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "emp_title_id"
+     )
 );
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("emp_title_id");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
@@ -57,7 +72,4 @@ REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "saleries" ADD CONSTRAINT "fk_saleries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "employees" ("emp_title_id");
 
